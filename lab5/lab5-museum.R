a02c102ba9ecff4cda22965364dcbc3bdf30fc64
S0LARANA <s0169513@edu.kubsu.ru>
2024-05-21 10:51:51 +0300

    lab5

--- lab5/lab5-museum.R
+++ lab5/lab5-museum.R
@@ -7,18 +7,15 @@ page <- read_html(url)
 museums_header <- page %>%
   html_nodes(xpath = "//h3[span[@id='.D0.9C.D1.83.D0.B7.D0.B5.D0.B8'] or span[@id='Музеи']]")
 
-following_elements <- museums_header %>%
-  html_nodes(xpath = "following-sibling::*[following-sibling::h3[span[@id='Театры и концертные залы']]]")
+theater_header <- page %>%
+  html_nodes(xpath = "//h3[span[@id='Театры и концертные залы'] or span[@id='.D0.A2.D0.B5.D0.B0.D1.82.D1.80.D1.8B_.D0.B8_.D0.BA.D0.BE.D0.BD.D1.86.D0.B5.D1.80.D1.82.D0.BD.D1.8B.D0.B5_.D0.B7.D0.B0.D0.BB.D1.8B']]")
 
-uls <- following_elements %>%
-  html_nodes("ul")
+uls <- page %>%
+  html_nodes(xpath = "//h3[span[@id='.D0.9C.D1.83.D0.B7.D0.B5.D0.B8'] or span[@id='Музеи']]/following-sibling::ul[preceding-sibling::h3[span[@id='Театры и концертные залы'] or span[@id='.D0.A2.D0.B5.D0.B0.D1.82.D1.80.D1.8B_.D0.B8_.D0.BA.D0.BE.D0.BD.D1.86.D0.B5.D1.80.D1.82.D0.BD.D1.8B.D0.B5_.D0.B7.D0.B0.D0.BB.D1.8B']]]")
 
 li_texts <- uls %>%
   html_nodes("li") %>%
   html_text()
 
 print(li_texts)
-
-
-
 
